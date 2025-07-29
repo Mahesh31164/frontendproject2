b5deef1b25ed55c0232bf5a786db0dfc28a81775
Mahesh31164 <addamahesh052@gmail.com>
2025-07-29 15:22:03 +0530

    frontend

---
+++ src/Page1.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello EVERY ONE </h1>
+        <h4> MY NAME IS MAHESH </h4>
+      </div>
+    )
+  }
+}
---
+++ src/Page2.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Link} from'react-router-dom'
+import './css/Page2.css'
+
+export default class Page2 extends Component {
+  render() {
+    return (
+     <div className='main1'>
+          <div className='link'>
+           <div className='link2'> 
+            <h1>Hello EVERY ONE</h1>
+            <h2>My name is mahesh</h2>
+          </div>
+          </div>
+      </div>
+ 
+
+    )
+  }
+}
